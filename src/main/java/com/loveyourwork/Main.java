5473e146da371cd3abd273750add8a2da93d8996
ants-byte <adiserio@gmail.com>
2025-09-04 18:00:15 +1000

    another one wq :wq :wq

--- src/main/java/com/loveyourwork/Main.java
+++ src/main/java/com/loveyourwork/Main.java
@@ -2,7 +2,8 @@ package com.loveyourwork;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world"); // trying again
+        System.out.println("Hello world"); // trying again
         System.out.println("Goodbye world"); // I have pushed this change
     }
 }
